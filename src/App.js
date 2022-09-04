38b1220b4023de9d7603c719472058cc8c556190
Benjamin Leong E-Jenn <leongejenn@1utar.my>
2022-09-04 23:59:18 +0800

    Change BrowserRouter to HashRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import { GlobalStyle } from "./globalStyle";
 import Hero from "./components/Hero";
 import Products from "./components/Products";
